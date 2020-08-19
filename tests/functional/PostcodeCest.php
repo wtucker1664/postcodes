8c0b21c8429bd24930317dbbe7bb463db998d41b
Warren Tucker <wtucker1664@me.com>
2020-08-19 15:49:19 +0100

    Tried to add runSymfonyConsoleCommand to functional testing but not working

--- tests/functional/PostcodeCest.php
+++ tests/functional/PostcodeCest.php
@@ -9,13 +9,18 @@ class PostcodeCest
     }
 
     // tests
-    public function postcodePageTest(FunctionalTester $I)
+  //  public function commandAppPostcodeTest(FunctionalTester $I)
+   // {
+        //$s = $I->runSymfonyConsoleCommand("app:import-postcodes",["args" => "/var/www/html/2020-05.zip"]); // Not working issue with run runSymfonyConsoleCommand it looks like
+  //  }
+    
+     public function postcodePageTest(FunctionalTester $I)
     {
         $I->amOnPage("/postcode/AA");
         $I->canSee("postcode");
     }
     
-    public function postcodePageLatLanTest(FunctionalTester $I)
+    public function postcodePageLatLonTest(FunctionalTester $I)
     {
         $I->amOnPage("/postcode/57.101474/-2.242851");
         $I->canSee("lat");
